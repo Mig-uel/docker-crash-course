a7cedbd8dbb3d1c3db83980a010e6e2fe72a5b2b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-29 16:46:07 -0400

    Update PORT definition to use environment variable
    - Allow dynamic port assignment via process.env.PORT
    - Maintain default to 3000 if not set

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const crypto = require('crypto')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(cors())
